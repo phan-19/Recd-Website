371e29a38c350988ec80a2d222f1ac96923c7b6a
juleskay-b <juliabush00@gmail.com>
2025-06-16 15:55:32 -0400

    Set up vite and react

---
+++ backend/target/debug/deps/backend-265b333f7ec1494c.d
@@ -0,0 +1,5 @@
+C:\Users\Jules\Projects\CEN_3031_Project\backend\target\debug\deps\libbackend-265b333f7ec1494c.rmeta: src\main.rs
+
+C:\Users\Jules\Projects\CEN_3031_Project\backend\target\debug\deps\backend-265b333f7ec1494c.d: src\main.rs
+
+src\main.rs:
---
+++ backend/target/debug/deps/backend-9aa4e50f4f093fe9.d
@@ -0,0 +1,5 @@
+C:\Users\Jules\Projects\CEN_3031_Project\backend\target\debug\deps\libbackend-9aa4e50f4f093fe9.rmeta: src\main.rs
+
+C:\Users\Jules\Projects\CEN_3031_Project\backend\target\debug\deps\backend-9aa4e50f4f093fe9.d: src\main.rs
+
+src\main.rs:
