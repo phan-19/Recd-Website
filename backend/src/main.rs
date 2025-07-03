f7ca7604af9a732b355109be3c6400210bc48416
PJacob0524 <PJacob0524@gmail.com>
2025-07-02 22:18:55 -0400

    fixed main directory set up and started on database schtuff

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::{migrate::MigrateDatabase, sqlite::SqliteQueryResult, Sqlite, SqlitePool};
+use std::env;
+use std::result::Result;
+
+async fn add_user(
+    pool: &SqlitePool,
+    username: &str,
+    password: &str,
+) -> Result<SqliteQueryResult, sqlx::Error> {
+}
+
+async fn list_users(
+    pool: &SqlitePool,
+) -> Result<SqliteQueryResult, sqlx::Error> {
+}
+
+async fn main() {
+    let args = env::args().collect();
+    let pool = SqlitePool::connect("sqlite://recd.db").await().unwrap();
 }
--- backend/target/debug/deps/backend-033977b62bc95543.d
+++
@@ -1,5 +0,0 @@
-C:\Users\PJaco\SWEProj\backend\target\debug\deps\libbackend-033977b62bc95543.rmeta: src\main.rs
-
-C:\Users\PJaco\SWEProj\backend\target\debug\deps\backend-033977b62bc95543.d: src\main.rs
-
-src\main.rs:
